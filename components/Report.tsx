30983b482480550a84e774f51cd6f02f27a670d3
JohnnySprit <flanegt@gmail.com>
2026-06-23 21:21:05 -0400

    added Report.tsx that displays final report information

---
+++ components/Report.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { PlayerSummary } from '@/lib/trimmer'
+import ReactMarkdown from 'react-markdown'
+
+interface ReportProps {
+    matchId: string
+    player: PlayerSummary
+    report: string
+}
+
+export default function Report({ matchId, player, report }: ReportProps) {
+    return (
+        <div className="flex flex-col gap-4 max-w-2xl mx-auto px-8 py-16 space-y-8">
+            <h1 className="text-sm text-zinc-400">Report: {matchId}</h1>
+            <p className="text-lg font-semibold text-white">Player: {player.summonerName} - {player.champion} - {player.role}</p>
+            <p className="text-lg font-semibold text-white"> Kills: {player.kills} - Deaths: {player.deaths} - Assists: {player.assists}</p>
+            <div className="text-zinc-300 leading-relaxed">
+                <ReactMarkdown>{report}</ReactMarkdown>
+            </div>
+        </div>
+    )
+}
